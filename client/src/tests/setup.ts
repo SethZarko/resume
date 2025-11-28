3dcf88b3bb7ee6ac04ed3ee6eadf1c55766a476f
SethZarko <98277879+SethZarko@users.noreply.github.com>
2025-11-27 22:00:09 -0500

    Fix tests after installing Framer Motion - add global mock observer to remove reference error (#11)

--- client/src/tests/setup.ts
+++ client/src/tests/setup.ts
@@ -5,3 +5,16 @@ import '@testing-library/jest-dom/vitest'
 afterEach(() => {
   cleanup();
 })
+
+class MockIntersectionObserver {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(_callback?: IntersectionObserverCallback, _options?: IntersectionObserverInit) {
+  }
+  observe() { /* do nothing */ }
+  disconnect() { /* do nothing */ }
+  unobserve() { /* do nothing */ }
+  takeRecords(): IntersectionObserverEntry[] { return []; }
+}
+
+window.IntersectionObserver = MockIntersectionObserver as unknown as typeof IntersectionObserver;
+global.IntersectionObserver = MockIntersectionObserver as unknown as typeof IntersectionObserver;
